f126d59801d027e8a561e779d0e27a934bede848
Ricardo S. Rocker <76121782+ricardorocker@users.noreply.github.com>
2020-12-21 00:21:29 -0300

    Add files via upload

---
+++ 04 - Tipo Dados/script.js
@@ -0,0 +1,26 @@
+//Bollean
+// null
+// undefined
+// Number
+// String
+
+var nome;
+var cpf;
+var idade1;
+var idade2;
+var vazio;
+var indefinido;
+
+nome = "Ricardo S. Rocker";
+cpf = "445.785.478-04";
+idade1 = 25;
+idade2 = 24;
+vazio = null;
+indefinido = undefined;
+
+
+console.log(idade1 + idade2);
+console.log("25" + "24");
+console.log(nome, " CPF:", cpf);
+console.log(vazio);
+console.log(undefined);
