5311cb5d2fcb313999a0058cf1d6eb1fb96234d6
Ricardo S. Rocker <76121782+ricardorocker@users.noreply.github.com>
2020-12-21 12:09:11 -0300

    Add files via upload

---
+++ 07 - Operadores Comparação/script.js
@@ -0,0 +1,23 @@
+//  == igual a
+//  === valor e tipo igual a
+//  != diferente de
+//  !== valor e tipo diferente de 
+//  > maior que
+//  < menor que
+//  >= maior ou igual a
+// <= menor ou igual a 
+
+var a = 25
+var b = 24
+var c = "25"
+
+console.log(a, " é igual == a ", b, ": ", a == b)
+console.log(a, " é igual == a ", c, ": ", a == c)
+console.log(a, " é igual === a ", c, ": ", a === c)
+console.log(a, " é diferente != de ", b, ": ", a != b)
+console.log(a, " é diferente != de ", c, ": ", a != c)
+console.log(a, " é diferente !== de ", c, ": ", a !== c)
+console.log(a, " é maior > que ", b, ": ", a > b)
+console.log(a, " é menor < que ", b, ": ", a < b)
+console.log(a, " é maior ou igual >= que ", a, ": ", a >= a)
+console.log(a, " é menor ou igual >= que ", a, ": ", a <= a)
