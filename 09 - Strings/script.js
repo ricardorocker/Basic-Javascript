fcf3a728e40eaec240b6d2b1e85683953713a8dd
Ricardo S. Rocker <76121782+ricardorocker@users.noreply.github.com>
2020-12-21 15:40:04 -0300

    Add files via upload

---
+++ 09 - Strings/script.js
@@ -0,0 +1,16 @@
+var nome = "Ricardo S. Rocker"
+var idade = 25
+
+var str =  "Usuario " + nome + " de " + idade
+str += " anos, cadastrado com sucesso!"
+
+var aspaSimples = "texto entre 'aspas' simples"
+
+var aspasDuplas = 'texto entre "aspas" duplas'
+
+
+console.log(str)
+console.log(aspaSimples)
+console.log(aspasDuplas)
+
+
