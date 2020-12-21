8edb329ad1472c62365f43e8f75cf150fa5d45c6
Ricardo S. Rocker <76121782+ricardorocker@users.noreply.github.com>
2020-12-21 17:38:17 -0300

    Add files via upload

---
+++ 11 - Ternário/script.js
@@ -0,0 +1,12 @@
+var idade = 121;
+
+var status = idade >= 18 ? "Maior" : "Menor";
+
+console.log(idade, "anos.");
+console.log("Erro: ", erro);
+
+var texto = idade > 25 ? console.log("Mais velho do que eu!") : console.log("Mais novo do que eu!");
+var erro = idade < 0 || idade > 120 ? true : false;
+
+console.log(status, "de idade");
+
