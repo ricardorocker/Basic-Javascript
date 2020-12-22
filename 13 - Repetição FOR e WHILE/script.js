fed3263f80d40e9746c41ece055aaca6de7953b1
Ricardo S. Rocker <76121782+ricardorocker@users.noreply.github.com>
2020-12-22 20:12:03 -0300

    Add files via upload

---
+++ 13 - Repetição FOR e WHILE/script.js
@@ -0,0 +1,23 @@
+// ------- FOR -------
+var numero = 7
+
+for (var i = 1; i < 8; i++){
+    console.log("'FOR' realizado pela " + i + "º vez.")
+}
+
+console.log("Execução finalizada!")
+console.log("")
+
+
+
+
+
+// ------- WHILE -------
+var i = 1
+
+while (i < 8){
+    console.log("'WHILE' realizado pela " + i + "º vez.")
+    i++
+}
+
+console.log("Execução de 'WHILE' finalizada!")
