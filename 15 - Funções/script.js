b15674fa9d80c705e6e8b94d4a2c9b665c10ee36
Ricardo S. Rocker <76121782+ricardorocker@users.noreply.github.com>
2020-12-23 13:36:15 -0300

    Add files via upload

---
+++ 15 - Funções/script.js
@@ -0,0 +1,15 @@
+function media(n1, n2, n3){
+var nota1 = n1
+var nota2 = n2
+var nota3 = n3
+
+var media = (nota1 + nota2 + nota3) / 3
+// console.log("A média deste aluno é " + media)
+return media
+}
+
+var resultado1 = media(9, 4, 7)
+var resultado2 = media(4, 5 ,10)
+
+console.log("A primeira média foi de " + resultado1 + "e a segunda média foi de " + resultado2)
+
