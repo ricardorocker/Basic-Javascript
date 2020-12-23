42e80b075c13f0b8489fcd12923bd691f82abb44
Ricardo S. Rocker <76121782+ricardorocker@users.noreply.github.com>
2020-12-23 12:54:28 -0300

    Add files via upload

--- 14 - Array/script.js
+++ 14 - Array/script.js
@@ -1,23 +1,42 @@
-// ------- FOR -------
-var numero = 7
+//  ------- METHOD 1 -------
+var aluno1 = "Ricardo"
+var aluno2 = "Rosangela"
+var aluno3 = "Amanda"
+var aluno4 = "Denis"
+var aluno5 = "Monique"
+console.log("Metodo sem 'Array & For'")
+console.log(aluno1)
+console.log(aluno2)
+console.log(aluno3)
+console.log(aluno4)
+console.log(aluno5)
 
-for (var i = 1; i < 8; i++){
-    console.log("'FOR' realizado pela " + i + "º vez.")
+//  ------- METHOD 2 -------
+var alunos = new Array("Ricardo", "Rosangela", "Amanda", "Denis", "Monique")
+console.log("Metodo com 'Array'")
+console.log(alunos[0])
+console.log(alunos[1])
+console.log(alunos[2])
+console.log(alunos[3])
+console.log(alunos[4])
+
+//  ------- METHOD 3 -------
+var alunos = new Array("Ricardo", "Rosangela", "Amanda", "Denis", "Monique")
+console.log("Metodo com 'Array & For'")
+for (var i = 0; i < alunos.length; i++) {
+    console.log(alunos[i])
 }
 
-console.log("Execução finalizada!")
-console.log("")
-
-
-
-
-
-// ------- WHILE -------
-var i = 1
-
-while (i < 8){
-    console.log("'WHILE' realizado pela " + i + "º vez.")
-    i++
+//  ------- METHOD 4 -------
+var alunos = new Array("Ricardo", "Rosangela", "Amanda", "Denis", "Monique")
+console.log("Metodo com 'Array & For(in)'")
+for (var aluno in alunos) {
+    console.log(alunos[aluno])
 }
 
-console.log("Execução de 'WHILE' finalizada!")
+//  ------- METHOD 5 -------
+var alunos = new Array("Ricardo", "Rosangela", "Amanda", "Denis", "Monique")
+console.log("Metodo com 'Array & For(of)'")
+for (var aluno of alunos) {
+    console.log(aluno)
+}
