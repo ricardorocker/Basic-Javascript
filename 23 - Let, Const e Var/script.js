90ffd71989a41caf74f488fcc8b971fd060691eb
Ricardo S. Rocker <76121782+ricardorocker@users.noreply.github.com>
2020-12-26 15:04:15 -0300

    Add files via upload

---
+++ 23 - Let, Const e Var/script.js
@@ -0,0 +1,23 @@
+var numero1 = 3
+let numero2 = 4
+const numero3 = 5 //Constante
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+
+
+// Constante nao permite mudar valor dela
+const numero4 = 5
+numero4 = 6
+console.log(numero4)
+
+// LET e CONST só são usadas dentro do bloco
+{
+var numero1 = 3
+let numero2 = 4
+const numero3 = 5 //Constante
+}
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
